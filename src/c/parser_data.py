ca11001ac3495aedc612f371d2963c15c1fd5f8d
Mihai Maruseac <mmaruseac@ixiacom.com>
2011-07-20 18:22:14 +0300

    Parsing include declarations

--- src/c/parser_data.py
+++ src/c/parser_data.py
@@ -1,14 +1,52 @@
 # IxCode - app for code spelunking :: block diagram
 # Data for parsing C code.
 
+import ast
+
 # Construct lang_lex_dict dictionary
 lang_parse_dict = {}
 
 # start symbol
-lang_parse_dict['start'] = 'foo'
+lang_parse_dict['start'] = 'external_decl'
 
-def p_foo(self, p):
-    'foo    :   PPHASH'
-    raise SyntaxError
-lang_parse_dict['p_foo'] = p_foo
+def p_external_decl_1(self, p):
+    'external_decl  :   empty'
+    print p.slice
+lang_parse_dict['p_external_decl_1'] = p_external_decl_1
+
+def p_external_decl_2(self, p):
+    'external_decl  :   pp_directive external_decl'
+    print p.slice
+lang_parse_dict['p_external_decl_2'] = p_external_decl_2
+
+def p_pp_directive(self, p):
+    'pp_directive   :   PPHASH INCLUDE LT included GT'
+    p[0] = ast.Include(p[4])
+    print p[0]
+lang_parse_dict['p_pp_directive'] = p_pp_directive
+
+def p_included_1(self, p):
+    'included   :   empty'
+    p[0] = ""
+lang_parse_dict['p_included_1'] = p_included_1
+
+def p_included_2(self, p):
+    'included   :   PERIOD included'
+    p[0] = p[1] + p[2]
+lang_parse_dict['p_included_2'] = p_included_2
+
+def p_included_3(self, p):
+    'included   :   DIVIDE included'
+    p[0] = p[1] + p[2]
+lang_parse_dict['p_included_3'] = p_included_3
+
+def p_included_4(self, p):
+    'included   :   LINECONT included'
+    p[0] = p[1] + p[2]
+lang_parse_dict['p_included_4'] = p_included_4
+
+def p_included_5(self, p):
+    'included   :   ID included'
+    p[0] = p[1] + p[2]
+lang_parse_dict['p_included_5'] = p_included_5
 
