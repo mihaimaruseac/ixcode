74e63cee728fd4b9ddcb4132bc1dc4cd13ae4069
Mihai Maruseac <mmaruseac@ixiacom.com>
2011-07-25 16:33:40 +0300

    Construct parsing injection dictionary automatically.

--- src/c/ixcode.py
+++ src/c/ixcode.py
@@ -1,6 +1,17 @@
 # IxCode - app for code spelunking :: block diagram
 # C specific code version of the tool
 
+import sys
+
 from lexer_data import lang_lex_dict
-from parser_data import lang_parse_dict
+import parser_data
+
+_d = sys.modules['src.c.parser_data'].__dict__
+lang_parse_dict = {}
+for k in _d:
+    if k[:2] == 'p_':
+        lang_parse_dict[k] = _d[k]
+
+# Change here if not starting from `file'
+lang_parse_dict['start'] = 'file'
 
--- src/c/parser_data.py
+++ src/c/parser_data.py
@@ -3,12 +3,6 @@
 
 import ast
 
-# Construct lang_lex_dict dictionary
-lang_parse_dict = {}
-
-# start symbol
-lang_parse_dict['start'] = 'file'
-
 # debug
 import sys
 def _p(msg):
@@ -17,7 +11,6 @@ def _p(msg):
 def p_file_0(self, p):
     'file   :   empty'
     _p(p.slice)
-lang_parse_dict['p_file_0'] = p_file_0
 
 def p_file_1(self, p):
     'file   :   file PPHASH'
@@ -25,37 +18,30 @@ def p_file_1(self, p):
     Raise error if encountering this token.
     """
     raise SyntaxError
-lang_parse_dict['p_file_1'] = p_file_1
 
 def p_file_2(self, p):
     'file   :   file decl'
     _p(p.slice)
-lang_parse_dict['p_file_2'] = p_file_2
 
 def p_decl(self, p):
     'decl   :   decl_specs type_spec varlist SEMI'
     _p(p.slice)
-lang_parse_dict['p_decl'] = p_decl
 
 def p_decl_specs_1(self, p):
     'decl_specs :   empty'
     _p(p.slice)
-lang_parse_dict['p_decl_specs_1'] = p_decl_specs_1
 
 def p_decl_specs_2(self, p):
     'decl_specs :   decl_specs type_qualifier'
     _p(p.slice)
-lang_parse_dict['p_decl_specs_2'] = p_decl_specs_2
 
 def p_decl_specs_3(self, p):
     'decl_specs :   decl_specs storage_class'
     _p(p.slice)
-lang_parse_dict['p_decl_specs_3'] = p_decl_specs_3
 
 def p_decl_specs_4(self, p):
     'decl_specs :   decl_specs func_spec'
     _p(p.slice)
-lang_parse_dict['p_decl_specs_4'] = p_decl_specs_4
 
 def p_type_qualifier(self, p):
     """
@@ -64,7 +50,6 @@ def p_type_qualifier(self, p):
                     |   VOLATILE
     """
     _p(p.slice)
-lang_parse_dict['p_type_qualifier'] = p_type_qualifier
 
 def p_type_spec_1(self, p):
     """
@@ -80,22 +65,18 @@ def p_type_spec_1(self, p):
                 |   ID
     """
     _p(p.slice)
-lang_parse_dict['p_type_spec_1'] = p_type_spec_1
 
 def p_type_spec_2(self, p):
     'type_spec  :   LONG LONG'
     _p(p.slice)
-lang_parse_dict['p_type_spec_2'] = p_type_spec_2
 
 def p_type_spec_3(self, p):
     'type_spec  :   LONG INT'
     _p(p.slice)
-lang_parse_dict['p_type_spec_3'] = p_type_spec_3
 
 def p_type_spec_4(self, p):
     'type_spec  :   LONG LONG INT'
     _p(p.slice)
-lang_parse_dict['p_type_spec_4'] = p_type_spec_4
 
 def p_storage_class(self, p):
     """
@@ -106,77 +87,62 @@ def p_storage_class(self, p):
                     |   TYPEDEF
     """
     _p(p.slice)
-lang_parse_dict['p_storage_class'] = p_storage_class
 
 def p_func_spec(self, p):
     'func_spec  :   INLINE'
     _p(p.slice)
-lang_parse_dict['p_func_spec'] = p_func_spec
 
 def p_varlist_1(self, p):
     'varlist    :   variable'
     _p(p.slice)
-lang_parse_dict['p_varlist_1'] = p_varlist_1
 
 def p_varlist_2(self, p):
     'varlist    :   varlist COMMA variable'
     _p(p.slice)
-lang_parse_dict['p_varlist_2'] = p_varlist_2
 
 def p_variable_1(self, p):
     'variable   :   var_name'
     _p(p.slice)
-lang_parse_dict['p_variable_1'] = p_variable_1
 
 def p_variable_2(self, p):
     'variable   :   pointer variable'
     _p(p.slice)
-lang_parse_dict['p_variable_2'] = p_variable_2
 
 def p_variable_3(self, p):
     'variable   :   var_name array'
     _p(p.slice)
-lang_parse_dict['p_variable_3'] = p_variable_3
 
 def p_array_1(self, p):
     'array  :   LBRACKET array_expression RBRACKET'
     _p(p.slice)
-lang_parse_dict['p_array_1'] = p_array_1
 
 def p_array_2(self, p):
     'array  :   array LBRACKET array_expression RBRACKET'
     _p(p.slice)
-lang_parse_dict['p_array_2'] = p_array_2
 
 def p_array_expression_1(self, p):
     'array_expression   :   expression'
     _p(p.slice)
-lang_parse_dict['p_array_expression_1'] = p_array_expression_1
 
 def p_array_expression_2(self, p):
     'array_expression   :   TIMES'
     _p(p.slice)
-lang_parse_dict['p_array_expression_2'] = p_array_expression_2
 
 def p_array_expression_3(self, p):
     'array_expression   :   empty'
     _p(p.slice)
-lang_parse_dict['p_array_expression_3'] = p_array_expression_3
 
 def p_pointer(self, p):
     'pointer    :   TIMES decl_specs'
     _p(p.slice)
-lang_parse_dict['p_pointer'] = p_pointer
 
 def p_var_name(self, p):
     'var_name   :   ID'
     _p(p[1])
     _p(p.slice)
-lang_parse_dict['p_var_name'] = p_var_name
 
 def p_expression(self, p):
     'expression :   ID'
     _p(p[1])
     _p(p.slice)
-lang_parse_dict['p_expression'] = p_expression
 
