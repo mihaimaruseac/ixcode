b7ce75e3a92c2eb7dcc65617b11554e1f2f7e3b9
Mihai Maruseac <mmaruseac@ixiacom.com>
2011-07-26 16:14:46 +0300

    Handling return statements.
    Example file completely parsed.

--- src/c/parser_data.py
+++ src/c/parser_data.py
@@ -310,6 +310,10 @@ def p_instruction_7(self, p):
     'instruction    :   initializer SEMI'
     _p(p.slice)
 
+def p_instruction_8(self, p):
+    'instruction    :   RETURN  expression SEMI'
+    _p(p.slice)
+
 def p_label(self, p):
     'label  :   ID  COLON'
     _p(p.slice)
