4f9158b8201e72765e8acc29de442a48ece00572
Mihai Maruseac <mmaruseac@ixiacom.com>
2011-07-27 12:21:33 +0300

    Constructing AST.

--- src/ast.py
+++ src/ast.py
@@ -1,6 +1,40 @@
 # IxCode - app for code spelunking :: block diagram
 # Generic AST.
 
+class File():
+    """
+    A file. Contains the list of functions.
+    """
+    def __init__(self):
+        self._fcts = {}
+
+    def add(self, f):
+        self._fcts['%s' % f.name()] = f
+
+    def filter(self, functions=[]):
+        """
+        Filters the ast to contain only interesting functions. If functions = []
+        no filtering is done.
+        """
+        if functions:
+            d = {}
+            for f in functions:
+                d[f] = self._fcts[f]
+            self._fcts = d
+
+    def __iter__(self):
+        """
+        Returns the stored functions, one by one.
+        """
+        for f in self._fcts:
+            yield (f, self._fcts[f])
+
+    def __str__(self):
+        return 'File: %s' % self._fcts
+
+    def __repr__(self):
+        return "-->" + self.__str__() + "<--"
+
 class Node:
     """
     Base class for all nodes in AST. All methods raise exceptions to ensure
@@ -9,4 +43,124 @@ class Node:
     def __str__(self):
         raise NotImplementedError()
 
+    def __repr__(self):
+        return "-->" + self.__str__() + "<--"
+
+class Block(Node):
+    """
+    A block of several instructions.
+    """
+    def __init__(self):
+        self._instructions = []
+
+    def add(self, i):
+        self._instructions.append(i)
+
+    def __str__(self):
+        return "<block> {...}"
+
+class Function(Node):
+    """
+    A function.
+    """
+    def __init__(self, name, header, arg, block):
+        """
+        name    -   name of function
+        header  -   declaration (inline int from inline int f(int a, int b))
+        arg     -   arguments (int a, int b from above decl)
+        block   -   content
+        """
+        self._text = '%s %s(%s){...}' % (header, name, arg)
+        self._name = name
+        self._block = block
+
+    def __str__(self):
+        return self._text
+
+    def name(self):
+        return self._name
+
+class TextNode(Node):
+    """
+    A node containing a text describing the corresponding instruction.
+    """
+    def __init__(self, text):
+        self._text = text
+
+    def __str__(self):
+        return self._text
+
+class Expression(TextNode):
+    """
+    A simple expression, a subpart of an instruction.
+    """
+    pass
+
+class Instruction(TextNode):
+    """
+    An instruction to be displayed in the diagram. Doesn't jump.
+    """
+    pass
+
+class RetInstruction(Instruction):
+    """
+    A return instruction. Always in a single block.
+    """
+    pass
+
+class ForInstruction(Instruction):
+    """
+    A for instruction. Causes a jump.
+    """
+    def __init__(self, header, content):
+        Instruction.__init__(self, "for %s {...}" % header)
+        self._content = content
+
+class GoToInstruction(Instruction):
+    """
+    A goto instruction. Causes a jump.
+    """
+    def __init__(self, label):
+        """
+        label - where to jump
+        """
+        Instruction.__init__(self, "goto %s" % label)
+        self._label = label
+
+class LabelInstruction(Instruction):
+    """
+    A label. Always the start of a new block.
+    """
+    def __init__(self, label):
+        """
+        label - text of label
+        """
+        Instruction.__init__(self, "%s:" % label)
+        self._label = label
+
+class BlockInstruction(Instruction):
+    """
+    A {...} block. Always a new block.
+    """
+    def __init__(self, block):
+        Instruction.__init__(self, "{...}")
+        self._block = block
+
+class IfInstruction(Instruction):
+    """
+    An if. Will always create two blocks.
+    """
+    def __init__(self, cond, true, false):
+        Instruction.__init__(self, "if (%s){...}" % cond)
+        self._true = true
+        self._false = false
+
+class MacroLoopInstruction(Instruction):
+    """
+    A macro which represents a loop. Guessed.
+    """
+    def __init__(self, header, body):
+        Instruction.__init__(self, "#loop %s{...}" % header)
+        self._header = header
+        self._body = body
 
--- src/c/parser_data.py
+++ src/c/parser_data.py
@@ -1,7 +1,7 @@
 # IxCode - app for code spelunking :: block diagram
 # Data for parsing C code.
 
-import ast
+import src.ast as ast
 
 # debug
 import sys
@@ -21,7 +21,7 @@ precedence = (
 
 def p_file_0(self, p):
     'file   :   empty'
-    _p(p.slice)
+    p[0] = ast.File()
 
 def p_file_1(self, p):
     'file   :   file PPHASH'
@@ -32,31 +32,32 @@ def p_file_1(self, p):
 
 def p_file_2(self, p):
     'file   :   file decl'
-    _p(p.slice)
+    p[0] = p[1] # ignore global decls
 
 def p_file_3(self, p):
     'file   :   file function'
-    _p(p.slice)
+    p[0] = p[1]
+    p[0].add(p[2])
 
 def p_decl(self, p):
     'decl   :   decl_specs type_spec varlist SEMI'
-    _p(p.slice)
+    p[0] = None # ignore global decls
 
 def p_decl_specs_1(self, p):
     'decl_specs :   empty'
-    _p(p.slice)
+    p[0] = ast.TextNode('')
 
 def p_decl_specs_2(self, p):
     'decl_specs :   decl_specs type_qualifier'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s %s' % (p[1], p[2]))
 
 def p_decl_specs_3(self, p):
     'decl_specs :   decl_specs storage_class'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s %s' % (p[1], p[2]))
 
 def p_decl_specs_4(self, p):
     'decl_specs :   decl_specs func_spec'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s %s' % (p[1], p[2]))
 
 def p_type_qualifier(self, p):
     """
@@ -64,7 +65,7 @@ def p_type_qualifier(self, p):
                     |   RESTRICT
                     |   VOLATILE
     """
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_storage_class(self, p):
     """
@@ -74,11 +75,11 @@ def p_storage_class(self, p):
                     |   EXTERN
                     |   TYPEDEF
     """
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_func_spec(self, p):
     'func_spec  :   INLINE'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_type_spec_1(self, p):
     """
@@ -93,72 +94,70 @@ def p_type_spec_1(self, p):
                 |   UNSIGNED
                 |   ID
     """
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_type_spec_2(self, p):
-    'type_spec  :   LONG LONG'
-    _p(p.slice)
+    """
+    type_spec   :   LONG LONG
+                |   LONG INT
+    """
+    p[0] = ast.TextNode('long %s' % p[2])
 
 def p_type_spec_3(self, p):
-    'type_spec  :   LONG INT'
-    _p(p.slice)
-
-def p_type_spec_4(self, p):
     'type_spec  :   LONG LONG INT'
-    _p(p.slice)
+    p[0] = ast.TextNode('long long int')
 
 def p_varlist_1(self, p):
     'varlist    :   variable'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_varlist_2(self, p):
     'varlist    :   varlist COMMA variable'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s, %s' % (p[1], p[2]))
 
 def p_variable_1(self, p):
     'variable   :   var_name'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_variable_2(self, p):
     'variable   :   pointer variable'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s %s' % (p[1], p[2]))
 
 def p_variable_3(self, p):
     'variable   :   var_name array'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s%s' % (p[1], p[2]))
 
 def p_var_name(self, p):
     'var_name   :   ID'
-    _p(p[1])
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_pointer(self, p):
     'pointer    :   TIMES decl_specs %prec POINTER'
-    _p(p.slice)
+    p[0] = ast.TextNode('*%s' % p[2])
 
 def p_array_1(self, p):
     'array  :   LBRACKET array_expression RBRACKET'
-    _p(p.slice)
+    p[0] = ast.TextNode('[%s]' % p[2])
 
 def p_array_2(self, p):
     'array  :   array LBRACKET array_expression RBRACKET'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s[%s]' % (p[1], p[3]))
 
 def p_array_expression_1(self, p):
     'array_expression   :   expression'
-    _p(p.slice)
+    p[0] = p[1]
 
 def p_array_expression_2(self, p):
     'array_expression   :   TIMES'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_array_expression_3(self, p):
     'array_expression   :   empty'
-    _p(p.slice)
+    p[0] = ast.TextNode('')
 
 def p_expression_1(self, p):
     'expression :   arg'
-    _p(p.slice)
+    p[0] = ast.Expression("%s" % p[1])
 
 def p_expression_2(self, p):
     """
@@ -173,11 +172,11 @@ def p_expression_2(self, p):
                 |   expression DIVIDE expression
                 |   expression MOD expression
     """
-    _p(p.slice)
+    p[0] = ast.Expression("%s %s %s" % (p[1], p[2], p[3]))
 
 def p_expression_3(self, p):
     'expression  :   MINUS arg %prec UMINUS'
-    _p(p.slice)
+    p[0] = ast.Expression("-%s" % p[2])
 
 def p_expression_4(self, p):
     """
@@ -186,31 +185,31 @@ def p_expression_4(self, p):
                 |   PLUSPLUS arg
                 |   MINUSMINUS arg
     """
-    _p(p.slice)
+    p[0] = ast.TextNode("%s%s" % (p[1], p[2]))
 
 def p_arg_1(self, p):
     'arg   :   arg_name'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_arg_2(self, p):
     'arg   :   TIMES arg %prec POINTER'
-    _p(p.slice)
+    p[0] = ast.TextNode('*%s' % p[2])
 
 def p_arg_3(self, p):
     'arg   :   arg_name array'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s%s' % (p[1], p[2]))
 
 def p_arg_4(self, p):
     'arg    :   function_call'
-    _p(p.slice)
+    p[0] = p[1]
 
 def p_arg_name_1(self, p):
     'arg_name   :   ID'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_arg_name_2(self, p):
     'arg_name   :   constant'
-    _p(p.slice)
+    p[0] = p[1]
 
 def p_constant_1(self, p):
     """
@@ -220,126 +219,129 @@ def p_constant_1(self, p):
                 |   INT_CONST_OCT
                 |   FLOAT_CONST
     """
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_function_call(self, p):
     'function_call  :   func_name LPAREN arglist RPAREN'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s(%s)' % (p[1], p[3]))
 
 def p_func_name(self, p):
     'func_name  :   ID'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_arglist_0(self, p):
     'arglist    :   empty'
-    _p(p.slice)
+    p[0] = ast.TextNode('')
 
 def p_arglist_1(self, p):
     'arglist    :   arg'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s' % p[1])
 
 def p_arglist_2(self, p):
     'arglist    :   arglist COMMA arg'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s, %s' % (p[1], p[3]))
 
 def p_function_0(self, p):
     'function   :   decl_specs type_spec func_name LPAREN f_arg_list RPAREN SEMI'
-    _p(p.slice)
+    p[0] = None # ignoring prototypes
 
 def p_function_1(self, p):
     'function   :   decl_specs type_spec func_name LPAREN f_arg_list RPAREN block'
-    _p(p.slice)
+    p[0] = ast.Function(p[3], '%s %s' % (p[1], p[2]), '%s' % p[5], p[7])
 
 def p_f_arg_list_1(self, p):
     'f_arg_list :   empty'
-    _p(p.slice)
+    p[0] = ast.TextNode('')
 
 def p_f_arg_list_2(self, p):
     'f_arg_list :   decl_specs type_spec variable'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s%s %s' % (p[1], p[2], p[3]))
 
 def p_f_arg_list_3(self, p):
     'f_arg_list :   f_arg_list COMMA decl_specs type_spec variable'
-    _p(p.slice)
+    p[0] = ast.TextNode('%s, %s%s %s' % (p[1], p[3], p[4], p[5]))
 
 def p_block(self, p):
     'block  :   LBRACE block_content RBRACE'
-    _p(p.slice)
+    p[0] = ast.BlockInstruction(p[2])
 
 def p_block_0(self, p):
     'block_content  :   empty'
-    _p(p.slice)
+    p[0] = ast.Block()
 
 def p_block_content_1(self, p):
     'block_content  :   block_content decl'
-    _p(p.slice)
+    p[0] = p[1] # TODO: ignore decls?
+    p[0].add(p[2])
 
 def p_block_content_2(self, p):
     'block_content  :   block_content instruction'
-    _p(p.slice)
+    p[0] = p[1]
+    p[0].add(p[2])
 
 def p_block_content_3(self, p):
     'block_content  :   block_content if'
-    _p(p.slice)
+    p[0] = p[1]
+    p[0].add(p[2])
 
 def p_instruction_1(self, p):
     'instruction    :   block'
-    _p(p.slice)
+    p[0] = ast.BlockInstruction(p[1])
 
 def p_instruction_2(self, p):
     'instruction    :   function_call instruction'
-    _p(p.slice)
+    p[0] = ast.MacroLoopInstruction(p[1], p[2])
 
 def p_instruction_3(self, p):
     'instruction    :   expression SEMI'
-    _p(p.slice)
+    p[0] = ast.Instruction("%s;" % p[1])
 
 def p_instruction_4(self, p):
     'instruction    :   label'
-    _p(p.slice)
+    p[0] = ast.LabelInstruction(p[1])
 
 def p_instruction_5(self, p):
     'instruction    :   GOTO ID SEMI'
-    _p(p.slice)
+    p[0] = ast.GoToInstruction(p[2])
 
 def p_instruction_6(self, p):
     'instruction    :   FOR for_header instruction'
-    _p(p.slice)
+    p[0] = ast.ForInstruction(p[2], p[3])
 
 def p_instruction_7(self, p):
     'instruction    :   initializer SEMI'
-    _p(p.slice)
+    p[0] = ast.Instruction("%s;" % p[1])
 
 def p_instruction_8(self, p):
     'instruction    :   RETURN  expression SEMI'
-    _p(p.slice)
+    p[0] = ast.RetInstruction("return %s;" % p[2])
 
 def p_label(self, p):
-    'label  :   ID  COLON'
-    _p(p.slice)
+    'label  :   ID COLON'
+    p[0] = ast.TextNode("%s" % p[1])
 
 def p_if_1(self, p):
     'if :   IF LPAREN expression RPAREN instruction'
-    _p(p.slice)
+    p[0] = ast.IfInstruction(p[3], p[5], None)
 
 def p_if_2(self, p):
     'if :   IF LPAREN expression RPAREN if'
-    _p(p.slice)
+    p[0] = ast.IfInstruction(p[3], p[5], None)
 
 def p_if_3(self, p):
     'if :   IF LPAREN expression RPAREN instruction ELSE instruction'
-    _p(p.slice)
+    p[0] = ast.IfInstruction(p[3], p[5], p[7])
 
 def p_if_4(self, p):
     'if :   IF LPAREN expression RPAREN instruction ELSE if'
-    _p(p.slice)
+    p[0] = ast.IfInstruction(p[3], p[5], p[7])
 
 def p_for_header_1(self, p):
     'for_header :   LPAREN initializer SEMI expression SEMI expression RPAREN'
+    p[0] = ast.TextNode("(%s; %s; %s)" % (p[2], p[4], p[6]))
     # TODO: other for expressions
-    _p(p.slice)
 
 def p_initializer(self, p):
     'initializer    :   arg EQUALS expression'
-    _p(p.slice)
+    p[0] = ast.Expression('%s = %s' % (p[1], p[3]))
 
