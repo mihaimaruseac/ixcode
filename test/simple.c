9556629838bd0b17f71bf4e4604ace6aa53255f9
Mihai Maruseac <mmaruseac@ixiacom.com>
2011-07-29 11:19:21 +0300

    Add if test. Reformat.

--- test/simple.c
+++ test/simple.c
@@ -3,64 +3,172 @@
  */
 // This is a single line comment
 #include <stdio.h>          //An include
-	#include <stdlib.h>
+#include <stdlib.h>
 
 #define SIZE 10
 #define for_each_start(i, start, size)\
-    for (i = start; i < size; i++)
+	for (i = start; i < size; i++)
 #define for_each(i, size)\
-    for_each_start(i, 0, size)
+	for_each_start(i, 0, size)
 
 int a[SIZE];
 
 void f1()
 {
-    int i;
+	int i;
 
-    for_each(i, SIZE)
-        printf("%d ", a[i]);
-    printf("\n");
+	for_each(i, SIZE)
+		printf("%d ", a[i]);
+	printf("\n");
 }
 
 void f2()
 {
-    int i;
+	int i;
 
 again:
-    for_each_start(i, a[0], SIZE){
-        printf("%d ", a[i]);
-        if (a[i] < a[0])
-            goto incr;
-    }
-    goto end;
+	for_each_start(i, a[0], SIZE){
+		printf("%d ", a[i]);
+		if (a[i] < a[0])
+			goto incr;
+	}
+	goto end;
 incr:
-    a[i]++;
-    printf("\n");
-    goto again;
+	a[i]++;
+	printf("\n");
+	goto again;
 end:
-    printf("\n");
+	printf("\n");
 }
 
 void f3()
 {
-    int i;
+	int i;
 
-    for (i = 0; i < SIZE; i++)
-        printf("%d ", a[i]);
-    printf("\n");
+	for (i = 0; i < SIZE; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
+void ifs()
+{
+	int i, j, k;
+
+	if (0) {
+		if (i < 3)
+			j++;
+
+		if (i < 4)
+			j++;
+		else
+			k++;
+
+		if (i < 4)
+			if (j < 5)
+				j++;
+			else
+				k++;
+
+		if (i < 4)
+			if (j < 5)
+				j++;
+			else
+				k++;
+		else
+			k--;
+
+		if (i < 4)
+			k--;
+		else
+			if (j < 5)
+				j++;
+			else
+				k++;
+
+		if (i < 4)
+			if (j < 5)
+				j--;
+			else
+				k--;
+		else
+			if (j < 5)
+				j++;
+			else
+				k++;
+	} else {
+		if (i < 3) {
+			j++;
+		}
+
+		if (i < 4) {
+			j++;
+		} else {
+			k++;
+		}
+
+		if (i < 4) {
+			if (j < 5) {
+				j++;
+			} else {
+				k++;
+			}
+		}
+
+		if (i < 4) {
+			if (j < 5) {
+				j++;
+			}
+		} else {
+			k++;
+		}
+
+		if (i < 4) {
+			if (j < 5) {
+				j++;
+			} else {
+				k++;
+			}
+		} else {
+			k--;
+		}
+
+		if (i < 4) {
+			k--;
+		} else {
+			if (j < 5) {
+				j++;
+			} else {
+				k++;
+			}
+		}
+
+		if (i < 4) {
+			if (j < 5) {
+				j--;
+			} else {
+				k--;
+			}
+		} else {
+			if (j < 5) {
+				j++;
+			} else {
+				k++;
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
-    int i;
+	int i;
 
-    a[0] = SIZE - 5;
-    for (i = 1; i < SIZE; i++)
-        a[i] = a[i - 1] + 2;
-    a[5] = 0;
-    f1();
-    f2();
-    f3();
-    return 0;
+	a[0] = SIZE - 5;
+	for (i = 1; i < SIZE; i++)
+		a[i] = a[i - 1] + 2;
+	a[5] = 0;
+	f1();
+	f2();
+	f3();
+	return 0;
 }
 
